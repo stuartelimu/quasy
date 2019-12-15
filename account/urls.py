2e7bb658b47d616ba7a394029ef9f0b3a1d96ea9
ElimuStuart <stuartelimu@gmail.com>
2019-12-15 17:02:21 +0300

    add user registration functionality

--- account/urls.py
+++ account/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
-    # path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
+    path('', views.dashboard, name='dashboard'),
     # path('login/', auth_views.LoginView.as_view(), name='login'),
     # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     # path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
